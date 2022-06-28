d286a991b5526ad836175de2dcbec684873c582c
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2022-06-28 14:18:18 -0300

    Adicionado sugestão de placeholder
    Adicionado sugestão de placeholder `%titansbattle_last_<killer|winner>_<game>%`

--- src/main/java/me/roinujnosde/titansbattle/hooks/papi/TBExpansion.java
+++ src/main/java/me/roinujnosde/titansbattle/hooks/papi/TBExpansion.java
@@ -34,7 +34,7 @@ public class TBExpansion extends PlaceholderExpansion {
         LAST_WINNER_GROUP_PATTERN = Pattern.compile("last_winner_group_(?<game>\\S+)");
         LAST_WINNER_KILLER_PATTERN = Pattern.compile("last_(?<type>winner|killer)_(?<game>\\S+)");
         PREFIX_PATTERN = Pattern.compile("(?<game>^\\S+)_(?<type>winner|killer)_prefix");
-        PLACEHOLDERS = Arrays.asList("%titansbattle_arena_in_use_<arena>%", "%titansbattle_last_winner_group_<game>%",
+        PLACEHOLDERS = Arrays.asList("%titansbattle_arena_in_use_<arena>%", "%titansbattle_last_<killer|winner>_<game>%", "%titansbattle_last_winner_group_<game>%",
                 "%titansbattle_<game>_<killer|winner>_prefix%", "%titansbattle_group_total_victories%",
                 "%titansbattle_total_kills%", "%titansbattle_total_deaths%");
     }
