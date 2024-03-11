c769281140e8147096beac8c703139ab8e2e9a95
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2024-03-11 03:17:39 -0300

    Resolvido problema ao modificar lista final
    Close #55

--- src/main/java/me/roinujnosde/titansbattle/commands/completions/ArenasCompletion.java
+++ src/main/java/me/roinujnosde/titansbattle/commands/completions/ArenasCompletion.java
@@ -6,9 +6,9 @@ import me.roinujnosde.titansbattle.TitansBattle;
 import me.roinujnosde.titansbattle.challenges.ArenaConfiguration;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ArenasCompletion extends AbstractCompletion {
     public ArenasCompletion(TitansBattle plugin) {
@@ -23,15 +23,18 @@ public class ArenasCompletion extends AbstractCompletion {
     @Override
     public Collection<String> getCompletions(BukkitCommandCompletionContext context) throws InvalidCommandArgument {
         List<String> inUse = getChallengeManager().getRequests().stream()
-                .map(cr -> cr.getChallenge().getConfig().getName()).toList();
+                .map(cr -> cr.getChallenge().getConfig().getName())
+                .toList();
+
         if (context.hasConfig("in_use")) {
             return inUse;
         }
 
         final boolean group = Boolean.parseBoolean(context.getConfig("group"));
-        List<String> arenas = getConfigurationDao().getConfigurations(ArenaConfiguration.class).stream()
-                .filter(a -> a.isGroupMode() == group).map(ArenaConfiguration::getName)
-                .toList();
+        List<String> arenas = new ArrayList<>(getConfigurationDao().getConfigurations(ArenaConfiguration.class).stream()
+                .filter(a -> a.isGroupMode() == group)
+                .map(ArenaConfiguration::getName)
+                .toList());
 
         arenas.removeAll(inUse);
         return arenas;
