3b19083cce19210b86f919e19e12f4a701b38de3
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2024-03-14 09:00:37 -0300

    Verificar se o warrior está no tempo de preparação

--- src/main/java/me/roinujnosde/titansbattle/listeners/PlayerMoveListener.java
+++ src/main/java/me/roinujnosde/titansbattle/listeners/PlayerMoveListener.java
@@ -2,21 +2,26 @@ package me.roinujnosde.titansbattle.listeners;
 
 import me.roinujnosde.titansbattle.BaseGame;
 import me.roinujnosde.titansbattle.TitansBattle;
+import me.roinujnosde.titansbattle.managers.DatabaseManager;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class PlayerMoveListener extends TBListener {
+    private final DatabaseManager dm;
 
     public PlayerMoveListener(@NotNull TitansBattle plugin) {
         super(plugin);
+        this.dm = plugin.getDatabaseManager();
     }
 
     @EventHandler(ignoreCancelled = true)
-    public void onPlayerMove(PlayerMoveEvent event) {
+    public void onPlayerMove(@NotNull PlayerMoveEvent event) {
         if (event.hasChangedBlock()) {
-            BaseGame game = plugin.getBaseGameFrom(event.getPlayer());
-            if (game != null && game.isPreparation()) {
+            Player player = event.getPlayer();
+            BaseGame game = plugin.getBaseGameFrom(player);
+            if (game != null && game.isPreparation() && game.getCurrentFighters().contains(dm.getWarrior(player))) {
                 event.setCancelled(true);
             }
         }
