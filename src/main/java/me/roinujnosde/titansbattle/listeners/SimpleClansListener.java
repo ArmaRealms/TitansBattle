e9d69332ee614be9cbd4b665b118b66bd06ebeb7
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2024-08-26 13:56:50 -0300

    Adicionado listener para PreDisbandClanEvent (#73)

--- src/main/java/me/roinujnosde/titansbattle/listeners/SimpleClansListener.java
+++ src/main/java/me/roinujnosde/titansbattle/listeners/SimpleClansListener.java
@@ -3,11 +3,15 @@ package me.roinujnosde.titansbattle.listeners;
 import me.roinujnosde.titansbattle.TitansBattle;
 import me.roinujnosde.titansbattle.managers.GameManager;
 import me.roinujnosde.titansbattle.types.Group;
+import me.roinujnosde.titansbattle.types.Warrior;
+import net.sacredlabyrinth.phaed.simpleclans.events.PreDisbandClanEvent;
 import net.sacredlabyrinth.phaed.simpleclans.events.PrePlayerKickedClanEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class SimpleClansListener extends TBListener {
     private final GameManager gm;
@@ -19,6 +23,19 @@ public class SimpleClansListener extends TBListener {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPrePlayerKickedClan(PrePlayerKickedClanEvent event) {
+        gm.getCurrentGame().ifPresent(game -> {
+            if (game.getConfig().isGroupMode()) {
+                List<UUID> participants = game.getParticipants().stream().map(Warrior::getUniqueId).toList();
+                UUID target = event.getClanPlayer().getUniqueId();
+                if (participants.contains(target)) {
+                    event.setCancelled(true);
+                }
+            }
+        });
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPreDisbandClan(PreDisbandClanEvent event) {
         gm.getCurrentGame().ifPresent(game -> {
             if (game.getConfig().isGroupMode()) {
                 String clanTag = event.getClan().getTag();
